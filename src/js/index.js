a9ef22e9c73fad00823368e8757512907a14baaf
Jessmor2 <135068935+Jessmor2@users.noreply.github.com>
2023-07-29 19:16:42 +0000

    First Commit

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,17 @@ import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import "../styles/simpleCounter.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Home from "./component/home.jsx"
+
+let counter = 0;
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(function () {
+    ReactDOM.render(<Home counter={counter}/>, document.querySelector("#app"));
+    counter++;
+    console.log(counter)
+}, 1000);
+
